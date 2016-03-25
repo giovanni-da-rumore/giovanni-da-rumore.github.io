6c3289a82406c3fd65907ea870a0d668387dfc7e
Jeremy <jsl2184@gmail.com>
2016-03-24 21:04:27 -0400

    complete basic styling for v2

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
-  var video = document.getElementById("header-video")
-  video.currentTime = 8;
-  video.muted = true;
-  var endtime = 110;
-  video.addEventListener("timeupdate", function() {
-       if (this.currentTime >= endtime) {
-            this.pause();
-            this.currentTime = 8;
-            this.play();
-        }
-    }, false);
+  // var video = document.getElementById("header-video")
+  // video.currentTime = 8;
+  // video.muted = true;
+  // var endtime = 110;
+  // video.addEventListener("timeupdate", function() {
+  //      if (this.currentTime >= endtime) {
+  //           this.pause();
+  //           this.currentTime = 8;
+  //           this.play();
+  //       }
+  // 
+  //   }, false);
+  $(document).on('click', '.js-info', function () {
+      var pos = $('#contact').position().top;
+      $('html, body').animate({scrollTop: pos}, 500);
+  })
+  
+  
+  
+  
 });
 
-$(window).scroll(function() {
-  var position = $(window).scrollTop();
-  $sideBar = $('.left-sidebar');
-  if (position > 398) {
-    $sideBar.addClass("fixed");
-  } else if ($sideBar.hasClass("fixed")) {
-    $sideBar.removeClass("fixed");
-  }
-});
+// $(window).scroll(function() {
+//   var position = $(window).scrollTop();
+//   $sideBar = $('.left-sidebar');
+//   if (position > 398) {
+//     $sideBar.addClass("fixed");
+//   } else if ($sideBar.hasClass("fixed")) {
+//     $sideBar.removeClass("fixed");
+//   }
+// });
